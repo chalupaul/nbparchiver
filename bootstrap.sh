688b9e9e5f44a808b0c449f73635e9b9df95469d
chalupaul <me@chalupaul.com>
2023-11-10 16:43:56 -0600

    fixing bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ curl https://pyenv.run | bash
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+echo 'pyenv 1>/dev/null 2>&1 && eval "$(pyenv init -)"' >> ~/.bashrc
 
 exec "$SHELL"
 pyenv install $(cat .python-version)
